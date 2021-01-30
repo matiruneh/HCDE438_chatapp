f8c2746056cd7bfe8712049564446f159dbe390c
Mahlet Tiruneh <mtiruneh@uw.edu>
2021-01-30 13:31:58 -0800

    Assignment 5 updates

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-
+import { useState } from 'react';
 import './App.css';
+import TextInput from './Textinput.js';
 
 function App() {
-  return (<div className="App">
+  const [messages, setMessages] = useState([{text:'hello'}])
+
+  return <div className="App">
       <header className="header">
-      <div className="logo" />
-      MyChat
-    </header>
-    </div>
-  );
+        <div className="logo" />
+        MyChat
+      </header>
+        
+      <section className="message">
+        {messages[0].text}
+      </section>
+
+      <TextInput send={t=> setMessages([{text:t}])}/>
+
+    </div>;
 }
 
 export default App;
---
+++ src/Textinput.js
@@ -0,0 +1,33 @@
+import {useState} from 'react'
+
+function TextInput(props){
+    const [text, setText] = useState('')
+
+    function sendMessage(){
+        if(text==='') return // skip the function
+        props.send(text)
+        setText('')
+    }
+
+    function keyPressed(e){
+        if(e.key==='Enter'){
+            sendMessage()
+        }
+    }
+
+    return <footer className='footer'>
+        <input className='text-input'
+        placeholder='Write your message'
+        value={text}
+        onChange={e=> setText(e.target.value)}
+        onKeyPress={keyPressed}/>
+        
+        <button className='button' 
+        onClick={sendMessage}
+        disabled={!text}> 
+            SEND 
+        </button>
+    </footer>
+}
+
+export default TextInput
